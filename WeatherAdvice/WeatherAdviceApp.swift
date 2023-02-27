33cd8efbaa58b5eb68f6c2b0d1638706e3205c2b
Charles Conroy <conroycd@bc.edu>
2023-02-27 17:17:23 -0500

    Initial Commit

---
+++ WeatherAdvice/WeatherAdviceApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAdviceApp.swift
+//  WeatherAdvice
+//
+//  Created by Charley Conroy on 2/27/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAdviceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
